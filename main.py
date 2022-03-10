44fddaeedcb240ff273a5aded14a539b4ee6d3d9
Sunanda <sunanda1sripally@gmail.com>
2022-03-10 18:43:49 +0530

    session cookies added

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 ####################################################################
 ###############          Import packages         ###################
 ####################################################################
-from flask import Blueprint, render_template, flash
+from flask import session,Blueprint, render_template, flash
 from flask_login import login_required, current_user
+from datetime import timedelta
 from __init__ import create_app, db
+
 
 ####################################################################
 # our main blueprint
 main = Blueprint('main', __name__)
 ####################################################################
+# @main.before_request
+# def before_request():
+#     session.permanent = True
+#     app.permanent_session_lifetime = timedelta(minutes=.5)
+#     session.modified = True
+
 @main.route('/') 
 def index():
     return render_template("index.html")
@@ -17,7 +25,13 @@ def index():
 @main.route('/dashboard') 
 @login_required
 def dashboard():
-    return render_template('dashboard.html', name=(current_user.first_name+' '+current_user.last_name))
+    if current_user.role == 'Admin':
+        return render_template('dashboard_admin.html', name=(current_user.first_name+' '+current_user.last_name))
+    elif current_user.role == 'Educatee':
+        return render_template('dashboard_educatee.html', name=(current_user.first_name+' '+current_user.last_name))
+    elif current_user.role == 'Educator':
+        return render_template('dashboard_educator.html', name=(current_user.first_name+' '+current_user.last_name))
+
 
 ####################################################################
 app = create_app() 
