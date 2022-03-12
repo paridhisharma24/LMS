2a475fd1a9f6d941a79736d82689b255687b6db7
Paridhi <paridhi24071@gmail.com>
2022-03-12 14:26:46 +0530

    update readme

--- README.md
+++ README.md
@@ -1,13 +1,53 @@
-# LMS
-**How to pull:**
-git clone for first time
-then git pull
+# Learning Management system
 
-**How to push**
-After required update made... commit, review changes, git push
 
-**Download dependencies**
+## Description
+The Learning Management System is an EdTech application that helps in delivering and managing the course smoothly. The application helps the educator in conducting the course by providing interfaces for sharing the material for the courses, assignments. The main aim of this application is to increase the interaction between students and teachers through discussion forums, bots, and so on. This portal will also help the users to keep a track of deadlines and the upcoming sessions which will boost the flexibility of the work process and work management. In the current circumstances where the learning is shifted to remote mode, this application will provide a smoother experience to the educators and the educates by replicating the actual offline learning environment.
+
+
+## Table of Content
+1. [ How to download ](#run)
+2. [ Set up  a Virtual Environment ](#venv)
+3. [ Install dependencies ](#usage)
+4. [ Run the website ](#start)
+
+<a name="run"></a>
+## How to download
+1. Make sure you have Git properly setup using your GitHub account.
+2. Run the following command in terminal to clone this repository in your device locally:
+```
+git clone https://github.com/<your_username>/LMS.git
+```
+
+<a name="venv"></a>
+## Set up  a Virtual Environment
+1. Make sure you are in the same directory in which the project was cloned (for simplicity).
+2. Install pip (python 3):
+```
+sudo apt update
+```
+```
+sudo apt install python3-pip
+```
+3. Setup virtual environment:
+```
+pip install <venv_name>
+```
+4. Activate the virtual environment (make sure you are in the same directory in which venv is created):
+```
+source <venv_name>/bin/activate
+```
+
+<a name="Install dependencies"></a>
+## Install dependencies
+1. To make sure all the packages are installed properly and up-to-date with this project, run:
+```
 pip install -r requirements.txt
+```
 
-**Run app**
-python main.py
+<a name="start"></a>
+## Run the Website
+1. In the terminal, type:
+```
+python3 main.py
+```
--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from __init__ import db
 from flask_login import UserMixin
 
 #available user roles: Admin, Mentor, Mentee 
-class UserRoles(UserMixin,db.Model):
+class UserRoles(UserMixin, db.Model):
     __tablename__ = 'UserRoles'
 
     role = db.Column(
@@ -15,40 +15,17 @@ class UserRoles(UserMixin,db.Model):
         unique=True,
         nullable=False
     )
-    details = db.relationship('User')
+    login = db.relationship('LoginDetails')
+    #dictionary to map roles to id
 
 
-#user specific details
-class User(UserMixin,db.Model):
-    __tablename__ = 'User'
-    first_name = db.Column(
-        db.String(100),
-        nullable=False
-    )
-
-    last_name = db.Column(
-        db.String(100),
-        nullable=False
-    )
-    
-    phone_no = db.Column(
-        db.String(10),
-        nullable=False
-    )
-
-    dob = db.Column(
-        db.Date,
-        nullable=True
-    )
-
-    address = db.Column(
-        db.String(150),
-        nullable=True
-    )
+class LoginDetails(UserMixin, db.Model):
+    __tablename__ = 'LoginDetails'
 
     user_id = db.Column(
         db.Integer, 
-        primary_key=True
+        primary_key=True,
+        autoincrement=True
     ) 
 
     email = db.Column(
@@ -72,15 +49,104 @@ class User(UserMixin,db.Model):
         db.String(20),
         nullable=False
     )
-    student = db.relationship('Course_Students')
-    mentor = db.relationship('Course_Mentors')
+    user = db.relationship('User')
+
+
+#user specific details
+class User(UserMixin,db.Model):
+    __tablename__ = 'User'
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('LoginDetails.user_id'),
+        primary_key=True
+    )
+
+    first_name = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    dob = db.Column(
+        db.Date,
+        nullable=True
+    )
+    phone_no = db.relationship('PhoneNo')
+    address = db.relationship('Address')
+    student = db.relationship('CourseStudents')
+    mentor = db.relationship('CourseInstance')
+
+
+#phone numbers of users
+class PhoneNo(UserMixin, db.Model):
+    __tablename__ = 'PhoneNo'
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('User.user_id'),
+        primary_key=True
+    )
+    phone = db.Column(
+        db.String(15),
+        nullable=False,
+        primary_key=True
+    )
+
+
+#address of user
+class Address(UserMixin, db.Model):
+    __tablename__ = 'Address'
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('User.user_id'),
+        primary_key=True
+    )
+
+    house_no = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    street = db.Column(
+        db.String(20),
+        nullable=True
+    )
+
+    locality = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    city = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    state = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    pincode= db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    country = db.Column(
+        db.String(50),
+        nullable=False
+    )
 
 
 #Available courses 
 class Course(UserMixin,db.Model):
     __tablename__ = 'Course'
     course_id = db.Column(
-        db.String(20),
+        db.Integer,
         nullable=False,
         primary_key=True
     )
@@ -89,32 +155,34 @@ class Course(UserMixin,db.Model):
         db.String(50),
         nullable=False
     )
-    content = db.relationship('Contents')
-    students = db.relationship('Course_Students')
-    mentor = db.relationship('Course_Mentors')
+    content = db.relationship('MentorContent')
+    students = db.relationship('CourseStudents')
+    mentor = db.relationship('CourseInstance')
 
 
 #types of content like Assignment, notes, lecture, etc.
 class ContentTypes(UserMixin,db.Model):
     __tablename__ = 'ContentTypes' 
+
     content_id = db.Column(
-        db.String(20),
+        db.Integer,
         nullable=False,
         primary_key = True
     )
-    ass_name = db.Column(
+
+    content_name = db.Column(
         db.String(50),
         unique=True,
         nullable=False
     )
-    content = db.relationship('Contents')
+    content = db.relationship('MentorContent')
 
 
-#content of a course
-#link is drive link where the assignment is uploaded
-class Contents(UserMixin,db.Model):
-    __tablename__ = 'Contents'
-    date_of_upload = db.Column(
+#contents of a course uploaded by a mentor - Assignment, Notes
+class MentorContent(UserMixin,db.Model):
+    __tablename__ = 'MentorContent'
+
+    upload_date = db.Column(
         db.Date,
         nullable=True
     )
@@ -126,65 +194,91 @@ class Contents(UserMixin,db.Model):
     )
 
     course_id = db.Column(
-        db.String(20),
+        db.Integer,
         db.ForeignKey('Course.course_id')
     )
     
-    content_type = db.Column(
-        db.String(20),
+    type = db.Column(
+        db.Integer,
         db.ForeignKey('ContentTypes.content_id')
     )
 
-    link = db.Column(
-        db.String(150),
+    data = db.Column(
+        db.LargeBinary,
         nullable=False
     )
-    assignment = db.relationship('Assignment')
+    assignment = db.relationship('MenteeAssignment')
 
 
 #student and course mapping
-class Course_Students(UserMixin,db.Model):
+class CourseStudents(UserMixin,db.Model):
     __tablename__ = 'Course_Students'
 
     course_id = db.Column(
-        db.String(20),
+        db.Integer,
         db.ForeignKey('Course.course_id'),
         primary_key = True
     )
-    user_id = db.Column(
+
+    student_id = db.Column(
         db.Integer,
         db.ForeignKey('User.user_id'),
         primary_key = True
     )
+
     grade = db.Column(
         db.String(10),
         nullable=True
     )
 
 
-#students and mentor mapping
-class Course_Mentors(db.Model):
-    __tablename__ = 'Course_Mentors'
+#course and mentor mapping
+class CourseInstance(db.Model):
+    __tablename__ = 'CourseInstance'
+
+    instance_id = db.Column(
+        db.Integer,
+        autoincrement=True,
+        primary_key = True
+    )
 
     course_id = db.Column(
-        db.String(20),
+        db.Integer,
         db.ForeignKey('Course.course_id'),
         primary_key = True
     )
-    user_id = db.Column(
+
+    mentor_id = db.Column(
         db.Integer,
         db.ForeignKey('User.user_id'),
         primary_key = True
+    )
+
+    start_date = db.Column(
+        db.Date,
+        nullable=True
+    )
+
+    end_date = db.Column(
+        db.Date,
+        nullable=True
+    )
+
+    upload_date = db.Column(
+        db.Date,
+        nullable=True
     )
 
 
 #details of assignment uploaded by students/mentees
-class Assignment(db.Model):
-    __tablename__ = 'Assignment'
+#mentee - assignment
+
+class MenteeAssignment(db.Model):  #submission
+    __tablename__ = 'MenteeAssignment'
 
     assignment_id = db.Column(
-        db.String(20),
-        db.ForeignKey('Contents.content_id'),
+        db.Integer,
+        db.ForeignKey('MentorContent.upload_id'),
         primary_key = True
     )
 
@@ -192,7 +286,7 @@ class Assignment(db.Model):
         db.Integer,
         db.ForeignKey('User.user_id'),
     )
-
+    
     link = db.Column(
         db.String(150),
         nullable=False
