cb0193ef72da9b89cf9b181098122e229f3c4bff
Navvv13 <navneetassi01331@gmail.com>
2022-03-13 16:54:42 +0530

    changed class names

--- admin.py
+++ admin.py
@@ -3,7 +3,7 @@ from flask import Blueprint,redirect,url_for,request,render_template
 from sqlalchemy import null
 from __init__ import db
 from flask_login import current_user
-from db_model import course_details, course_students, course_mentors
+from models import Course, CourseStudents, CourseInstance
 
 admin = Blueprint('admin',__name__)
 
@@ -15,7 +15,7 @@ def addCourse():
         print(request.form.get('courseName'))
         if(request.form.get('courseName') == ""):
             return render_template('addCourse.html', name=current_user.name,message="Please Enter a Valid Course Name")
-        new_course = course_details(
+        new_course = Course(
             course_name = request.form.get('courseName')
         )
         db.session.add(new_course)
@@ -28,7 +28,7 @@ def addStudent():
     if request.method == 'GET':
         return render_template('addStudent.html', name=current_user.name)
     else:
-        new_courseStudent = course_students(
+        new_courseStudent = CourseStudents(
             course_id = request.form.get('courseId'),
             user_id = request.form.get('studentID')
         )
@@ -45,7 +45,7 @@ def addInstructor():
     if request.method == 'GET':
         return render_template('addInstructor.html', name=current_user.name)
     else:
-        new_courseStudent = course_mentors(
+        new_courseStudent = CourseInstance(
             course_id = request.form.get('courseId'),
             user_id = request.form.get('studentID')
         )
