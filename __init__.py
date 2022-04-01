f262ba7f704d3f1aed29bf808622aacae19d628c
Palak Agarwal <palakag1410@gmail.com>
2022-04-02 00:45:26 +0530

    fixed main branch

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
+from sqlite3 import OperationalError
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from sqlalchemy import create_engine
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -14,9 +16,14 @@ def create_app():
     app.config["SESSION_PERMANENT"] = False
     app.config["SESSION_TYPE"] = "filesystem"
     Session(app)
+    
+    
+    try:
+        create_engine(app.config['SQLALCHEMY_DATABASE_URI']).connect()
+    except OperationalError:
+        db.create_all()
 
     db.init_app(app)
-    
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login' 
     login_manager.init_app(app)
