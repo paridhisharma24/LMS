0bbf2d5c57601407f0891545b6f1e3b3dcaa2c63
Palak Agarwal <palakag1410@gmail.com>
2022-04-01 20:33:50 +0530

    created a way to create db without pushing binary

--- __init__.py
+++ __init__.py
@@ -4,19 +4,20 @@ from flask_login import LoginManager
 from flask_session import Session
 
 db = SQLAlchemy()
+app = Flask(__name__)
 
 def create_app():
-    app = Flask(__name__)
+    
     app.config['SECRET_KEY'] = 'secret-key-goes-here' 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite' 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
     app.config["SESSION_PERMANENT"] = False
     app.config["SESSION_TYPE"] = "filesystem"
     Session(app)
-  
-    db.init_app(app) 
+
+    db.init_app(app)
     
-    login_manager = LoginManager() 
+    login_manager = LoginManager()
     login_manager.login_view = 'auth.login' 
     login_manager.init_app(app)
     from models import LoginDetails
